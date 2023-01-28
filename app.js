d575a969c392e0ba8315867f4a4338da6978e4f0
Nikki Dilling <ndillinghansen@gmail.com>
2023-01-29 01:58:39 +0900

    fixed reveal for all occurrences + mistakes count + hangman reveal

--- app.js
+++ app.js
@@ -5,10 +5,20 @@
 const wordContainer = document.querySelector(".word-container");
 const words = ["APPLE", "COMPUTER", "BLANKET", "SHOWERCURTAIN"];
 const alphabet = document.querySelector('.alphabet-container');
+let mistakeCountDisplay = document.querySelector('.mistakes-count-num');
 
 let letterArray = [];
+let letterArrayIndexes = [];
 let randomNumber = Math.floor((Math.random() * (3 - 0 + 1)) + 0);
 let wordToGuess = words[randomNumber];
+
+let mistakeCount = 0;
+
+function removeProperty() {
+    element =
+    document.styleSheets[0].cssRules[0].style;
+    element.removeProperty('text-decoration');
+}
 
 function setWord(){
 
@@ -39,12 +49,54 @@ function fillArray(){
     console.log(letterArray);
 }
 
+function showHangman(){
+
+    switch(mistakeCount){
+        case 1: 
+                document.querySelector('.bottom-line').style.display = 'block';
+                break;
+        case 2: 
+                document.querySelector('.horizontal-line').style.display = 'block';
+                break;
+        case 3:
+                document.querySelector('.vertical-line').style.display = 'block';
+                break;
+        case 4:
+                document.querySelector('.binding-line').style.display = 'block';
+                break;
+        case 5:
+                document.querySelector('.rope').style.display = 'block';
+                break;         
+        case 6:
+                document.querySelector('.head').style.display = 'block';
+                break;
+        case 7:
+                document.querySelector('.body').style.display = 'block';
+                break;
+        case 8:
+                document.querySelector('.left-arm').style.display = 'block';
+                break; 
+        case 9:
+                document.querySelector('.right-arm').style.display = 'block';
+                break;
+        case 10:
+                document.querySelector('.left-leg').style.display = 'block';
+                break;
+        case 11:
+                document.querySelector('.right-leg').style.display = 'block';
+
+                
+    }
+    console.log("(f)mistake count: " + mistakeCount);
+    mistakeCountDisplay.innerText = `${mistakeCount}`;
+}
 
 alphabet.addEventListener('click', function(e){
 
     console.log(e.target.innerText + " is pressed");
 
     if(letterArray.includes(`${e.target.innerText}`)){
+
         console.log(`letter ${e.target.innerText} exists`);
         console.log("index of letter: " + letterArray.indexOf(`${e.target.innerText}`));
 
@@ -52,18 +104,35 @@ alphabet.addEventListener('click', function(e){
         let lettersToGuess = document.querySelectorAll(".letter");
         console.log(lettersToGuess[0]);
 
-        for(let i=0; i < lettersToGuess.length; i++){
-
-            console.log("inside for loop");
-
-            if(i === letterArray.indexOf(`${e.target.innerText}`)){
-                lettersToGuess[i].innerText = e.target.innerText;
-                console.log('letter is placed');
+        //saving indexes of all occurences of the letter that is clicked into an array
+        for(let i = 0; i < letterArray.length; i++){
+            if(letterArray[i] === `${e.target.innerText}`){
+                letterArrayIndexes.push(i);
             }
         }
 
+//Checking indexes of letters and revealing letters of the word to the player
+        for(let i=0; i < letterArrayIndexes.length; i++){
+
+            for(let j = 0; j < lettersToGuess.length; j++){
+
+                if(j === letterArrayIndexes[i]){
+                    lettersToGuess[j].innerText = e.target.innerText;
+                    console.log('letter is placed at: ' + j);
+                    
+                }
+            }
+
+        }
+
+        letterArrayIndexes = []; //emptying the index array
+
     }else{
         console.log(`letter ${e.target.innerText} does not exist`);
+
+        mistakeCount++;
+        //function that displays the hangman drawing when a mistake is made
+        showHangman();
     }
 });
 
